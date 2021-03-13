14bf5fed7b474e479f5b9eb5182f7869766220e1
szaszpeti <szaszakpeti@gmail.com>
2021-03-13 15:45:26 +0100

    first commit

---
+++ entries/models.py
@@ -0,0 +1,74 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class Entry(models.Model):
+    
+    entry_date = models.DateTimeField(auto_now_add=True)
+    entry_location = models.CharField(max_length=50)
+    entry_wtgnumber = models.CharField(max_length=50)
+    entry_technician_one = models.ForeignKey(User, on_delete=models.CASCADE)
+    entry_technician_two = models.CharField(max_length=50)
+    entry_technician_three = models.CharField(max_length=50, blank=True)
+    entry_notes = models.TextField(blank=True)
+    
+    
+    
+    TRAVEL_OFFICE_WTG = 'Travel Office --> WTG'
+    TRAVEL_WTG_OFFICE = 'Travel WTG --> Office'
+    TRAVEL_HOTEL_WTG = 'Travel Hotel --> WTG'
+    TRAVEL_WTG_HOTEL = 'Travel WTG --> Hotel'
+    TRAVEL_EXTRA = 'Travel Extra'
+    WORK_PREPARATION = 'Work Preparation'
+    UT_INSPECTION = 'UT Inspection'
+    STANDBY_WEATHER = 'Standby Weather'
+    STANDBY_WTG_DISORDER = 'Standby WTG Disorder'
+    CLEANING = 'Cleaning'
+    OTHER = 'Other'
+    
+    
+    
+    ACTIVITY = [(TRAVEL_OFFICE_WTG, 'Travel Office --> WTG '),
+                         (TRAVEL_WTG_OFFICE, 'Travel WTG --> Office'),
+                         (TRAVEL_HOTEL_WTG, 'Travel Hotel --> WTG'),
+                         (TRAVEL_WTG_HOTEL, 'Travel WTG --> Hotel'),
+                         (TRAVEL_EXTRA, 'Travel Extra'),
+                         (WORK_PREPARATION, 'Work Preparation'),
+                         (UT_INSPECTION, 'UT Inspection'),
+                         (STANDBY_WEATHER, 'Standby Weather'),
+                         (STANDBY_WTG_DISORDER, 'Standby WTG Disorder'),
+                         (CLEANING, 'Cleaning'),
+                          (OTHER, 'Other'),
+                         
+                         ]
+
+
+
+    entry_activity_1 = models.CharField(choices=ACTIVITY, max_length=50)
+    entry_time_start_1 = models.TimeField(null=True,blank=True, auto_now=False, auto_now_add=False)
+    entry_time_end_1 = models.TimeField(null=True,blank=True, auto_now=False, auto_now_add=False)
+    
+    entry_activity_2 = models.CharField(choices=ACTIVITY, max_length=50)
+    entry_time_start_2 = models.TimeField(null=True,blank=True, auto_now=False, auto_now_add=False)
+    entry_time_end_2 = models.TimeField(null=True,blank=True, auto_now=False, auto_now_add=False)
+    
+    entry_activity_3 = models.CharField(choices=ACTIVITY, max_length=50)
+    entry_time_start_3 = models.TimeField(null=True,blank=True, auto_now=False, auto_now_add=False)
+    entry_time_end_3 = models.TimeField(null=True,blank=True, auto_now=False, auto_now_add=False)
+    
+    entry_activity_4 = models.CharField(choices=ACTIVITY, max_length=50)
+    entry_time_start_4 = models.TimeField(null=True,blank=True, auto_now=False, auto_now_add=False)
+    entry_time_end_4 = models.TimeField(null=True,blank=True, auto_now=False, auto_now_add=False)
+    
+   
+    
+    class Meta:
+        verbose_name_plural = "entries"
+        
+    def __str__(self):
+        return f'{self.entry_location + " - " + self.entry_wtgnumber}'
+    
+    
+
+    
+    
---
+++ entries/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Entry
+
+class HomeView(LoginRequiredMixin, ListView):
+    model = Entry
+    template_name = 'entries/index.html'
+    context_object_name = "timesheet_entries"
+    ordering = ['-entry_date']
+    paginate_by = 3
+    
+class EntryView(LoginRequiredMixin, DetailView):
+    model = Entry
+    template_name = 'entries/detail_entry.html'
+    
+    
+class CreateEntryView(LoginRequiredMixin, CreateView):
+    model = Entry
+    template_name = 'entries/create_entry.html'
+    fields = '__all__'
+    
+    
+    #author is the user
+    def form_valid(self,form):
+        form.instance.entry_author = self.request.user
+        return super().form_valid(form)
